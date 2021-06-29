a679300828ab8c4a4c8aaa1ea39957fbff264254
wdrik <w.drik@outlook.com>
2021-06-28 21:15:01 -0300

    feat: add sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,15 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Header />
-      <h1>
-        Dashboard <span>!!</span>
-      </h1>
-    </>
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
