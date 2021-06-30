93b3c8142192382ce561e471a2812599d2e523a3
wdrik <w.drik@outlook.com>
2021-06-30 13:25:08 -0300

    feat: add sidebar context

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, ReactNode, useContext } from 'react';
+import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/react';
+
+interface SidebarDrawerProviderProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export function SidebarDrawerProvider({
+  children,
+}: SidebarDrawerProviderProps) {
+  const disclosure = useDisclosure();
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
